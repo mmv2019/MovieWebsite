eec44326e88490fc2dc53914508cf8139e5e7ae9
Maryam Maged Victor <56774613+mmv2019@users.noreply.github.com>
2022-10-29 00:33:01 +0200

    deleted comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { MoviePageComponent } from './movie-page/movie-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomePageComponent,
+    MoviePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
